22ecbb1ae4427a0c07e70c24168df8d62ae0b445
BWurster <wursterben17@gmail.com>
2021-10-24 00:34:20 -0500

    build

---
+++ build/precache-manifest.f643163ccd8cc86a32406c0e872f6c9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f1882696c8b4b5b51683a1d1f7a630",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cb0033a427b6d9a011d",
+    "url": "/static/css/main.73a3fcf3.chunk.css"
+  },
+  {
+    "revision": "9abda34f1cdaa3d36c91",
+    "url": "/static/js/2.ac257dd5.chunk.js"
+  },
+  {
+    "revision": "f13b8ee62297cdeb55e53ad5d3f0bd7f",
+    "url": "/static/js/2.ac257dd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cb0033a427b6d9a011d",
+    "url": "/static/js/main.d453f954.chunk.js"
+  },
+  {
+    "revision": "9bc2349bfbe76e87395f",
+    "url": "/static/js/runtime-main.e5afff51.js"
+  }
+]);
