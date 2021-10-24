e8368c10e1a940faef141543a0c2079ca8e86f98
Prannav Arora <71447892+bluepra@users.noreply.github.com>
2021-10-23 23:02:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
-document.addEventListener("DOMContentLoaded", event => {
+// const firebase = require('firebase');
+// require('firebase/firestore');
+document.addEventListener("DOMContentLoaded", event => {    
     const app = firebase.app();
     const db = firebase.firestore();
 
-    const my_text = db.collection('texts').doc('text1');
+    // db.collection("texts").get().then((querySnapshot) => {
+    //     querySnapshot.forEach((doc) => {
+    //         // doc.data() is never undefined for query doc snapshots
+    //         document.querySelector('#title').innerHTML += doc.data().content + '<br>';
+    //     });
+    // });
 
-    my_text.onSnapshot(doc => {
-        const data = doc.data();
-        document.querySelector('#title').innerHTML = data.content;
+    // db.collection("texts").onSnapshot((doc) => {
+    //     document.querySelector('#title').innerHTML += doc.data().content + '<br>';
+    // });
+    
+    db.collection("texts").onSnapshot((querySnapshot) => {
+        document.querySelector('#title').innerHTML = '';
+        querySnapshot.forEach((doc) => {
+            document.querySelector('#title').innerHTML += doc.data().content + '<br>';
+        });
     });
+    
 });
 
+
+
 function addText(){
+    const textBox = document.querySelector('#text-box');
+    const newText = textBox.value;
+    textBox.value = '';
     const db = firebase.firestore();
-    const my_text = db.collection('texts').doc('text1');
-    my_text.update({content: "Chris"});
+    // const my_text = db.collection('texts').doc('text1');
+    // my_text.update({content: newText});
+    db.collection("texts").doc(newText).set({
+        content: newText
+    });
 };
